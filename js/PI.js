2073ea9939679cd27ee502f6d58c4e9fda1894c6
starbrucks172020-arch <starbrucks172020@gmail.com>
2026-05-07 15:22:40 -0300

    Update PI.js

--- js/PI.js
+++ js/PI.js
@@ -20,12 +20,12 @@ const guias = {
             " Quebrar os vidros em pedaços grandes(mão protegida)",
             "NÃO precisa secar completamente o vidro ",
         ]
- 
+
     },
- 
+
     papel: {
- 
- 
+
+
         titulo: "Papel",
         cor: "papel",
         aceito: [
@@ -34,15 +34,15 @@ const guias = {
             "Cadernos e folhas de sulfite  ",
             "Papel de escritório (sem grampos)"
         ],
- 
+
         naoAceito: [
             "Papel plastificado ou metalizado ",
             "Guardanapos e papéis seujos de comida",
             "Papéis carbonos e térmicos",
             "Fitas adesivas e etiquetas",
-       
+
         ],
- 
+
         dicas: [
             " Remover grampos,clipes e espirais metálicas",
             " Separar papel sulfite do papelão",
@@ -50,32 +50,32 @@ const guias = {
             "NÃO precisa estar seco ou limpo",
         ],
     },
- 
+
     plastico: {
         titulo: "Plástico",
         cor: "plastico",
         aceito: [
-        "Garrafas PET(água,refrigerante)",
-        "Embalagens de shampoo e detergente",
-        "Potinhos de margarina/requeijão",
-        "Sacos plásticos limpos(supermercado) ",
- 
+            "Garrafas PET(água,refrigerante)",
+            "Embalagens de shampoo e detergente",
+            "Potinhos de margarina/requeijão",
+            "Sacos plásticos limpos(supermercado) ",
+
         ],
         naoAceito: [
-         "Isopor (EPS) e embalagens de fast-food",
-         "Plástico sujo ou com comida",
-         " CDs,DvDs e canetas",
-         " Brinquedos e utensílios plásticos ",
+            "Isopor (EPS) e embalagens de fast-food",
+            "Plástico sujo ou com comida",
+            " CDs,DvDs e canetas",
+            " Brinquedos e utensílios plásticos ",
         ],
         dicas: [
-          "LAVAR SEMPRE (remove resíduo de comida)",
-          "Remover tampas e rotúlos ",
-          "Amassar garrafas para economizar espaço",
-          " Separar por cor se possivél (transparente/colorido)",
- 
+            "LAVAR SEMPRE (remove resíduo de comida)",
+            "Remover tampas e rotúlos ",
+            "Amassar garrafas para economizar espaço",
+            " Separar por cor se possivél (transparente/colorido)",
+
         ],
     },
- 
+
     organico: {
         titulo: "Orgânico",
         cor: "organico",
@@ -92,17 +92,17 @@ const guias = {
             " Frutas estragadas com mofo",
         ],
         dicas: [
-           "Corte em pedaços pequenos: Quanto menor os pedaços, mais rápido eles se decompõem! Corte cascas, legumes e restos de comida em tamanhos pequenos.",
-           "Retire embalagens: Tire todo plástico, papel ou metal que estiver junto com o alimento. Só coloque a matéria orgânica pura!.",
-           "Evite líquidos em excesso: Não coloque muita água ou molhos junto, pois pode estragar. O ideal é que esteja úmido, mais não encharcado.",
- 
+            "Corte em pedaços pequenos: Quanto menor os pedaços, mais rápido eles se decompõem! Corte cascas, legumes e restos de comida em tamanhos pequenos.",
+            "Retire embalagens: Tire todo plástico, papel ou metal que estiver junto com o alimento. Só coloque a matéria orgânica pura!.",
+            "Evite líquidos em excesso: Não coloque muita água ou molhos junto, pois pode estragar. O ideal é que esteja úmido, mais não encharcado.",
+
         ],
 
-        
+
     },
 
-    
-   compostagem: { 
+
+    compostagem: {
         titulo: "Compostagem Doméstica",
         cor: "organico",
         aceito: [
@@ -124,85 +124,70 @@ const guias = {
             "O composto estará pronto em cerca de 2 a 6 meses."
         ]
     }
-};
- 
- 
- 
- 
+}
+
 function montarConteudo(dados) {
     return `
- 
         <strong>O que é Aceito(Sim):</strong>
         <ul>
             ${dados.aceito.map(item => `<li>${item}</li>`).join('')}
         </ul>
- 
         <hr>
- 
         <strong>O que não é Aceito(Não):</strong>
         <ul>
             ${dados.naoAceito.map(item => `<li>${item}</li>`).join('')}
         </ul>
- 
         <hr>
- 
         <strong>Dicas de preparo:</strong>
         <ul>
             ${dados.dicas.map(item => `<li>${item}</li>`).join('')}
-       </ul>
-           
+       </ul>  
     `
- 
 }
- 
- 
- 
- 
+
+
 function openModal(tipo) {
     const modal = document.getElementById('modalInfo');
     const content = document.getElementById('modalContent');
     const title = document.getElementById('modalTitle');
     const modalCard = modal.querySelector('.modal-card');
- 
+
     const dados = guias[tipo];
     if (!dados) return;
- 
+
     modalCard.classList.remove('vidro', 'papel', 'plastico', 'organico');
     modalCard.classList.add(dados.cor);
- 
+
     title.innerText = `Detalhes de Resíduo: ${dados.titulo}`;
     content.innerHTML = montarConteudo(dados);
- 
+
     modal.classList.add('is-active');
 }
- 
- 
+
+
 function closeModal() {
     document.getElementById('modalInfo').classList.remove('is-active');
 }
- 
- 
+
+
 document.querySelectorAll('.btn-modal').forEach(btn => {
     btn.addEventListener('click', () => {
         openModal(btn.dataset.tipo);
     });
 });
- 
+
 document.getElementById('closeModal').addEventListener('click', closeModal);
 document.querySelector('.modal-background').addEventListener('click', closeModal);
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+
+
+/*parte da tela do Dicas Susténtaveis */
+function abrirConteudo(id) {
+
+    const conteudo = document.getElementById(id);
+
+    conteudo.classList.toggle("ativo");
+
+}
+
  
